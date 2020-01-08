53849b83299e009644f9a267a1880c7f22c77c3b
heyitsnovi <novhex94@gmail.com>
2020-01-08 15:05:42 +0800

    Update Summer Note

--- application/modules/user/views/summernote/index.php
+++ application/modules/user/views/summernote/index.php
@@ -1,17 +1,43 @@
-<div class="card">
-    <div class="card-header">
-        <h3 class="card-title"></h3>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
 
-        <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fas fa-minus"></i></button>
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title"></h3>
+
+                    <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                        <i class="fas fa-minus"></i></button>
+                    </div>
+                </div>
+                <div class="card-body">
+
+                    <form method="POST" action="">
+
+                        <div class="form-group col-md-6">
+                            <label for="note">Note Title</label>
+                            <input type="note" class="form-control" id="note" placeholder="Note Title">
+                        </div>
+                        
+                        <div class="form-group">
+                                <label for="summernote">Notes</label>
+                                <textarea id="summernote"></textarea>
+                        </div>
+                  
+
+                        <div class="form-group col-md-6">
+                            <button class="btn btn-success">Submit</button>
+                        </div>
+
+                    </form>
+
+                </div>
+
+                <!-- /.card-body -->
+                <div class="card-footer"></div>
+                <!-- /.card-footer-->
+            </div>
         </div>
     </div>
-    <div class="card-body">
-        <div id="summernote"></div>
-    </div>
-    <!-- /.card-body -->
-    <div class="card-footer"></div>
-    <!-- /.card-footer-->
 </div>
-<!-- /.card -->
