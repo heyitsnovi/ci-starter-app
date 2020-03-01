d301e71e509d0df08a1556a0cfae9aadd9e44133
kelnovi_94 <nohvz0514@gmail.com>
2020-03-01 19:37:58 +0800

    Fix HMVC router error

--- application/third_party/MX/Router.php
+++ application/third_party/MX/Router.php
@@ -235,10 +235,11 @@ class MX_Router extends CI_Router
 
 	public function set_class($class)
 	{
-		$suffix = $this->config->item('controller_suffix');
-		if (strpos($class, $suffix) === FALSE)
+		$suffix = strval($this->config->item('controller_suffix'));
+		$pos = !empty($suffix) ? strpos($class, $suffix) : FALSE;
+		if ($pos === FALSE)
 		{
-			$class .= $suffix;
+		    $class .= $suffix;
 		}
 		parent::set_class($class);
 	}
